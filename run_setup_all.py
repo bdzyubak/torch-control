5d1679f7d2a8eb9300eb331db6373707dc6bea06
Bogdan Dzyubak <illan7@gmail.com>
2024-03-26 15:58:29 -0500

    Fixed dependency issues.

--- run_setup_all.py
+++ run_setup_all.py
@@ -14,7 +14,7 @@ def main(envs_list=None):
     # want to pin specific versions of all dependencies.
 
     check_conda_installed()
-    path_ymls = Path(__file__).parent
+    path_dependencies = Path(__file__).parent
 
     envs = {'cv': {'install_method': 'environment_computer_vision.yml', 'submodules': ['nnUNet']},
             'nlp': {'install_method': 'environment_natural_language_processing.yml', 'submodules': []},
@@ -23,11 +23,14 @@ def main(envs_list=None):
         envs = {key: value for key, value in envs.items() if key in envs_list}
     develop_paths = False
 
+    # Install shared torch in base environment. Other environments will be able to see it if their required
+    # version is the same
+    run_command(f"pip install -r {path_dependencies / 'requirements.txt'}")
+
     for env_name in envs:
         run_command(f'conda remove -n {env_name} --all --y')
 
-        conda_create_from_yml(env_name=env_name, file=path_ymls / envs[env_name]['install_method'])
-        conda_extend_env(env_name=env_name, file=path_ymls / "environment_shared.yml")
+        conda_create_from_yml(env_name=env_name, file=path_dependencies / envs[env_name]['install_method'])
 
         if develop_paths:
             develop_submodules(env_name)
