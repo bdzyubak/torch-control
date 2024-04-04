7ee4ef59925f8db8a4a57e28df2b2cbe45644f0e
Bogdan Dzyubak <illan7@gmail.com>
2024-04-04 11:01:58 -0500

    Added integration test for LLM_pytorch_lighting_wrapper

--- projects/NaturalLanguageProcessing/MovieReviewAnalysis/analyze_training_logs.py
+++ projects/NaturalLanguageProcessing/MovieReviewAnalysis/analyze_training_logs.py
@@ -3,5 +3,7 @@ from pathlib import Path
 from plotting import plot_tensorboard_logs
 
 
-plot_tensorboard_logs(model_dir=Path(r"D:\Models\LLM\movie_sentiment_analysis"), model_version="version_7",
+plot_tensorboard_logs(model_dir=Path(r"D:\Models\LLM\movie_sentiment_analysis"), model_version="version_2",
                       title='Kaggle Movie Sentiment, DistilBERT Fine Tuning')
+
+
