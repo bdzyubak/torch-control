4e07e882e0dbbaa7da21df3d57ef186043be1689
Bogdan Dzyubak <illan7@gmail.com>
2024-04-09 13:22:53 -0500

    Split off tokenizer from the model into data loading

--- projects/NaturalLanguageProcessing/MovieReviewAnalysis/fine_tune_on_kaggle_movie_sentiment.py
+++ projects/NaturalLanguageProcessing/MovieReviewAnalysis/fine_tune_on_kaggle_movie_sentiment.py
@@ -6,10 +6,9 @@ import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
-from transformers import AutoTokenizer
-
-from utils.LLM_pytorch_lighting_wrapper import model_setup
+from utils.LLM_pytorch_lighting_wrapper import model_setup, tokenizer_setup
 from panda_utils import set_display_rows_cols, do_train_val_test_split, read_dataframe
+
 
 
 set_display_rows_cols()
@@ -22,15 +21,14 @@ def main(model_name, freeze_pretrained_weights=True):
     else:
         model_save_file = model_name + '_last_checkpoint.pth'
 
-    train_dataloader, val_dataloader = _set_up_dataloading()
+    train_dataloader, val_dataloader = _set_up_dataloading(tokenizer_name=model_name)
 
     model_save_dir = Path(r"D:\Models\LLM") / Path(__file__).stem
     model_save_dir.mkdir(exist_ok=True, parents=True)
     # Prefer to get number of classes procedurally, but this requires loading data. For now, hard specify to debug new
     # models. The clean solution is to load just one sample datapoint to get number of labels.
     model, trainer = model_setup(model_save_dir,
-                                 num_classes=5,
-                                 # num_classes=train_dataloader.dataset.__getitem__(0)['labels'].shape[0],
+                                 num_classes=train_dataloader.dataset.__getitem__(0)['labels'].shape[0],
                                  model_name=model_name)
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
 
@@ -40,7 +38,7 @@ def main(model_name, freeze_pretrained_weights=True):
     torch.save(model.state_dict(), model_save_dir / model_save_file)
 
 
-def _set_up_dataloading():
+def _set_up_dataloading(tokenizer_name):
     file_path = r"D:\data\SentimentAnalysisOnMovieReviews\train.tsv"
     # This dataset is heavily resampled with each review being split into smaller chunks down to one letter. The
     # smaller chunks seem to inherit the original review, so the target sentiment for "A"  and "A series",
@@ -52,14 +50,16 @@ def _set_up_dataloading():
     # file_path = r"D:\data\SentimentAnalysisOnMovieReviews\test.tsv"
     # df_test = read_dataframe(file_path)
     df_test, df_train, df_val = do_train_val_test_split(df)
-    train_dataloader, val_dataloader, test_dataloader = data_loading(df_train=df_train, df_val=df_val, df_test=df_test)
+    train_dataloader, val_dataloader, test_dataloader = data_loading(df_train=df_train, df_val=df_val, df_test=df_test,
+                                                                     tokenizer_name=tokenizer_name)
     return train_dataloader, val_dataloader
 
 
-def data_loading(df_train, df_val, df_test, subsample=None):
-    train_dataset = KaggleSentimentDataset(df_train, subsample=subsample)  # subsample = 1000 for debug
-    val_dataset = KaggleSentimentDataset(df_val, subsample=subsample)
-    test_dataset = KaggleSentimentDataset(df_test, subsample=subsample)
+def data_loading(df_train, df_val, df_test, tokenizer_name, subsample=None):
+    # subsample = 1000 for debug
+    train_dataset = KaggleSentimentDataset(df_train, tokenizer_name=tokenizer_name, subsample=subsample)
+    val_dataset = KaggleSentimentDataset(df_val, tokenizer_name=tokenizer_name, subsample=subsample)
+    test_dataset = KaggleSentimentDataset(df_test, tokenizer_name=tokenizer_name, subsample=subsample)
     print(f'The train/val/test split is: {len(train_dataset)}, {len(val_dataset)}, {len(test_dataset)}')
     # num_workers=3 is recommended by lightining. Depends on available resources and cpu.
     train_dataloader = DataLoader(train_dataset, batch_size=10, shuffle=True, num_workers=3, persistent_workers=True)
@@ -78,18 +78,12 @@ def one_hot_encode_sentiment(ratings: pd.Series) -> np.ndarray:
 
 
 class KaggleSentimentDataset(torch.utils.data.Dataset):
-    def __init__(self, df, subsample: int = None, tok: str = None):
+    def __init__(self, df, tokenizer_name, subsample: int = None):
         if subsample is None or subsample > len(df):
             subsample = len(df)
         self.df = df[:subsample]
 
-        if tok is None:
-            tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-        else:
-            try:
-                tokenizer = AutoTokenizer.from_pretrained(tok)
-            except:
-                raise ValueError(f'Unsupported tokenizer {tok}')
+        tokenizer = tokenizer_setup(tokenizer_name)
 
         self.examples = self.df['Phrase']
         self.encodings = tokenizer(self.df['Phrase'].to_list(), truncation=True, padding=True)
